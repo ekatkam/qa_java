226e25113192808ed101ecfd95e67d976172eda4
Ekaterina Kamzina <ekaterina_1407@mail.ru>
2024-07-30 16:41:28 +0300

    Exception tests fixed with assertThrows

--- src/test/java/ExceptionsTest.java
+++ src/test/java/ExceptionsTest.java
@@ -1,32 +1,29 @@
 import com.example.Animal;
+import com.example.Feline;
 import com.example.Lion;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertThrows;
+
+
 public class ExceptionsTest {
 
+    private Feline feline;
+
     @Test
-    public void getFoodExceptionTest() throws Exception {
+    public void getFoodExceptionTest() {
         Animal animal = new Animal();
-        String expected = "Неизвестный вид животного, используйте значение Травоядное или Хищник";
-        try {
-            animal.getFood("Неизвестно");
-        }
-        catch (Exception exception) {
-            Assert.assertEquals(expected, exception.getMessage());
-        }
+        Exception exception = assertThrows(
+                Exception.class, () ->
+                    animal.getFood("Неизвестно"));
+        Assert.assertEquals("Неизвестный вид животного, используйте значение Травоядное или Хищник", exception.getMessage());
     }
 
-    @Test
-    public void hasManeExceptionTest() {
-        String expected = "Используйте допустимые значения пола животного - самец или самка";
-         try {
-            Lion lion = new Lion("Неизвестно");
-        }
-        catch (Exception exception) {
-            Assert.assertEquals(expected, exception.getMessage());
-        }
+    @Test public void hasManeExceptionTest() {
+        Exception exception = assertThrows(
+                Exception.class, () -> {Lion lion = new Lion("Неизвестно", feline);
+                });
+        Assert.assertEquals("Используйте допустимые значения пола животного - самец или самка", exception.getMessage());
     }
-
-
-}
+    }
